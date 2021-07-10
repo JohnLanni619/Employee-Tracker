2f4edac0e069d0848f833c9c962aa322126eb8ba
John Lanni <Johnlanni619@gmail.com>
2021-07-10 16:26:45 -0400

    created sql files in db folder

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE department (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    FOREIGN KEY(department_id) REFERENCES department(department_id)
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    FOREIGN KEY(role_id) REFERENCES role(role_id),
+    manager_id INTEGER
+);
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO department (name)
+VALUES
+    ('Sales'),
+    ('Personal Training'),
+    ('Aquatics'),
+    ('Pilates'),
+    ('Customer Service'),
+    ('Nursing'),
+    ('Maintenance');
+
