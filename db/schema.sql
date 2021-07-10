f8cdcbe97e1f14f9db1106a29154ff734a63444e
John Lanni <Johnlanni619@gmail.com>
2021-07-10 16:39:53 -0400

    updated schema.sql tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
 CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
@@ -7,13 +11,15 @@ CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    FOREIGN KEY(department_id) REFERENCES department(department_id)
+    department_id INTEGER,
+    FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    FOREIGN KEY(role_id) REFERENCES role(role_id),
-    manager_id INTEGER
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY(role_id) REFERENCES role(id)
 );
