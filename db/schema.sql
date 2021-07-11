41187edff78c6a9bfbdcc63e8298404c1e2407d8
John Lanni <Johnlanni619@gmail.com>
2021-07-11 11:49:21 -0400

    updating schema and adding entries in seeds

--- db/schema.sql
+++ db/schema.sql
@@ -8,18 +8,19 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INTEGER,
-    FOREIGN KEY(department_id) REFERENCES department(id)
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+  CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER,
-    FOREIGN KEY(role_id) REFERENCES role(id)
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER,
+  CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES role(id),
+  CONSTRAINT fk_employee FOREIGN KEY(manager_id) REFERENCES employee(id)
 );
